5829453f88a3f5553cd5c2d7a811229bb3757be3
Laurita Radiševska <laurita.radisevska@mif.stud.vu.lt>
2024-11-19 19:46:55 +0100

    initial

---
+++ AndroidStudioProjects/BeerAdviserApp2/app/src/main/java/com/example/beeradviserapp2/MainActivity.kt
@@ -0,0 +1,44 @@
+ package com.example.beeradviserapp2
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    fun getBeers(color:String):List<String>{
+        return when(color){
+            "Light"-> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber"-> listOf("Jack Amber", "Red moose")
+            "Brown"->listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout","Dark Daniel")
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val findBeer=findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener{
+            val beerColor=findViewById<Spinner>(R.id.beer_color)
+            val color=beerColor.selectedItem
+            val beerList=getBeers(color.toString())
+            val beers=beerList.reduce{ str, item->str +'\n'+item}
+            val brands=findViewById<TextView>(R.id.brands)
+            brands.text=beers
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
